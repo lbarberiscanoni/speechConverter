e6c5e9d72a66a810e376c04698f72bfb9c2a9c51
lbarberiscanoni <hllbck7@gmail.com>
2015-05-12 04:42:23 -0700

    changed a few things to make the script more easily readable:

--- conversion.py
+++ conversion.py
@@ -5,23 +5,23 @@ import shlex
 import time
 
 # convert .docx into .markdown 
-fileName = raw_input("Type the name of the file EXACTLY and include the file extension\n :")
-year = raw_input("what year is it? Please use 4 numbers\n :")
-month = raw_input("what month is it? Please use 2 numbers\n :")
-day = raw_input("what day of the month is it? please use 2 numbers\n :")
+fileName = raw_input("Type the name of the file EXACTLY and include the file extension\n \nIf there are spaces, the use \ between each word\n \nExample: GMOs Neg.docx = GMOs\ Neg.docx\n \nEnter Here: ")
+year = raw_input("\nwhat year is it? Please use 4 numbers\n :")
+month = raw_input("\nwhat month is it? Please use 2 numbers\n :")
+day = raw_input("\nwhat day of the month is it? please use 2 numbers\n :")
 date = year + "-" + month + "-" + day + "-"
-speechTitle = raw_input("give title to this speech, without using capital letters and using - between each word\n :")
+speechTitle = raw_input("\ngive title to this speech, WITHOUT USING CAPITAL LETTERS and using - between each word\n :")
 speechName = date + speechTitle + ".markdown"
 
 subprocess.call("w2m " + fileName + " >>" + speechName, shell=True)
 
 # build the front matter and add it to the .markdown file
-n_of_tags = input("How many topic areas does your speech fit in?\n The options are:\n Constitutional Law (conlaw)\n, Counterplans (counterplan)\n, Domestic (domestic)\n, Economics (econ)\n, Foreign Policy (foreign)\n, Philosophical (philosophical)\n \nEnter the number (as a number and not a word) here :")
+n_of_tags = input("HOW MANY topic areas does your speech fit in?\n The options are:\n Constitutional Law (conlaw)\n, Counterplans (counterplan)\n, Domestic (domestic)\n, Economics (econ)\n, Foreign Policy (foreign)\n, Philosophical (philosophical)\n \nEnter the number (as a number and not a word) here :")
 time.sleep(1)
 
 tags = []
 for i in range(int(n_of_tags)):
-    choice = "\n- " + raw_input("\nChoose between conlaw, counterplan, domestic, econ, foreign, and philosophical\n \nEnter the topic area here :")
+    choice = "\n- " + raw_input("\nChoose between conlaw, counterplan, domestic, econ, foreign, and philosophical\n \nEnter the TOPIC AREA here :")
     tags.append(choice)
     time.sleep(1)
 
